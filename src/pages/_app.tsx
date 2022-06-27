ea588375b0cbc8754cb6e03660c4c75f0bf7620a
William Sharp <wsharp07@gmail.com>
2022-06-27 19:44:15 -0400

    more boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { CssVarsProvider, Sheet } from '@mui/joy';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CssVarsProvider>
+      <Sheet variant="outlined">
+        <Component {...pageProps} />
+      </Sheet>
+    </CssVarsProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
